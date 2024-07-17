d0181db54929f8fd11788a1d24a06a2355ad16ba
Francianeny <nirinasoabozy@yahoo.fr>
2024-07-18 01:47:46 +0200

    1-block-scoped.js

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,6 +1,10 @@
+// This function uses `export default` to allow it to be the default export of the module
 export default function taskBlock() {
+  // Declare two constants `task` and `task2` with boolean values
   const task = false;
   const task2 = true;
 
+  // Return an array containing the two constants
   return [task, task2];
 }
+
