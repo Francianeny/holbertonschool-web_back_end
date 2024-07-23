e1a5e54cdbe5b8f01ba5564677d120b0e422d15a
Francianeny <nirinasoabozy@yahoo.fr>
2024-07-23 16:42:13 +0200

    0-promise.js

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI() {
+  // La fonction retourne un nouvel objet Promise
+  return new Promise((resolve) => {
+    // La fonction resolve est appelée immédiatement, remplissant ainsi la promesse
+    resolve();
+  });
+}
