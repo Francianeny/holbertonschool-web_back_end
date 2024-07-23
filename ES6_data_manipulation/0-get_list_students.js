2dc83a9a3ef485eeeca4a9c45541e7972906e6ff
Francianeny <nirinasoabozy@yahoo.fr>
2024-07-23 14:34:11 +0200

    0-get_list_students.js

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+export default function getListStudents() {
+  // Define the array of student objects
+  const listStudent = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  // Return the array of student objects
+  return listStudent;
+}
