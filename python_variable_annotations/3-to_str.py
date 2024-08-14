6b83f576703cdead8555dc4074565129b2f9e31a
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-14 11:03:31 +0200

    3-to_str.py

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Converts a floating-point number to its string representation."""
 
-def to_str(n: float)-> str:
+
+def to_str(n: float) -> str:
+    """Returns:str: The string representation of the number."""
     return str(n)
