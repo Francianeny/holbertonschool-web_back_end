15b4c000a23ae7bf981a2eaa64590449309e2952
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-14 12:32:21 +0200

    8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-""" Multiplies the input value by the factor."""
+"""Provides a function to create a multiplier function with a
+specified factor."""
 from typing import Callable
 
 
 def make_multiplier(factor: float) -> Callable[[float], float]:
+    """Creates a multiplier function that multiplies its input by
+    the given factor."""
     def multiply(value: float) -> float:
-        """Returns float: The product of the input value and the factor."""
+        """Multiplies the input value by the factor."""
         return value * factor
+    """Returns float: The product of the input value and the factor."""
     return multiply
