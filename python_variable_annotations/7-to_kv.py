015851289940686eb0595ddf70f29c2341becaa3
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-14 12:06:38 +0200

    7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+"""Convert a key-value pair into a tuple where the value is squared
+and converted to float."""
 from typing import Union, Tuple
 
-def to_kv(k: str, v: Union[int, float])-> Tuple[str, float]:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns Tuple[str, float]: A tuple containing the key and the squared
+    value as a float."""
     return (k, float(v**2))
