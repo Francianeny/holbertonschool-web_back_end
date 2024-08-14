c7d11e1d0eb63bf4991ab888b5eceb0ebb9208a4
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-14 10:39:44 +0200

    2-floor.py

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import math
 
-def floor(n: float)-> int:
+
+def floor(n: float) -> int:
+    """Return int: The largest integer less than or equal to the number."""
     return math.floor(n)
