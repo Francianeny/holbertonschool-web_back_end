eaccb372f6d5b0ca86be5d774db69dd0ca4ec77d
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-14 10:22:47 +0200

    0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
+"""Adds two floating-point numbers."""
 
-# Add two blank lines before function definitions to comply with PEP 8 style guide
 
 def add(a: float, b: float) -> float:
-    """
-    Adds two floating-point numbers.
-
-    Parameters:
-    a (float): The first number.
-    b (float): The second number.
-
-    Returns:
-    float: The sum of the two numbers.
-    """
+    """return float: The sum of the two numbers."""
     return a + b
