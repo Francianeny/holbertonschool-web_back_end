accfef35a67eac22015bfc214c97b1242c5e2304
Francianeny <nirinasoabozy@yahoo.fr>
2024-08-14 10:28:07 +0200

    1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""Concatenates two strings."""
+
+
 def concat(str1: str, str2: str)-> str:
+    """return str: The concatenation of the two strings."""
     return str1 + str2
