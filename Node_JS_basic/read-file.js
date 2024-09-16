030db4a0723c91330537eea6f5bde93090cd3701
Francianeny <nirinasoabozy@yahoo.fr>
2024-09-16 23:24:53 +0200

    all to commit

--- Node_JS_basic/read-file.js
+++ Node_JS_basic/read-file.js
@@ -1,9 +1,24 @@
-const fs = require('fs');
+import { beforeEach, expect, test } from '@jest/globals';
 
-fs.readFile('database.csv', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
+const fs = require('fs');
+const path = require('path');
+
+let fileData;
+
+beforeEach((done) => {
+  // Remplacez 'database.csv' par le chemin réel si nécessaire
+  fs.readFile(path.join(__dirname, 'database.csv'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      done(err);
+      return;
+    }
+    fileData = data;
+    done();
+  });
+});
+
+test('should read file data', () => {
+  expect(fileData).toBeDefined();
+  expect(fileData).toContain('some expected content'); // Ajoutez ce que vous attendez dans le fichier
 });
