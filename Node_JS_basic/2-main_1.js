0058ffe4bbb53bf709b8bab3473bdf35f0e53f89
Francianeny <nirinasoabozy@yahoo.fr>
2024-09-20 00:31:03 +0200

    2-main_1.js

--- Node_JS_basic/2-main_1.js
+++ Node_JS_basic/2-main_1.js
@@ -1,6 +1,3 @@
 const countStudents = require('./2-read_file');
-const path = require('path');
 
-// Utilisation du chemin relatif pour accéder au fichier dans le sous-répertoire 'test'
-const filePath = path.resolve(__dirname, 'test', 'database.csv');
-countStudents(filePath);
+countStudents("database.csv");
